aa1cfbd2795b01eaf9215f1b8a20bbf22e730308
codingscode <heronnestg@hotmail.com>
2021-08-07 00:04:38 -0300

    parte 2

--- 0001teste1/programa.c
+++ 0001teste1/programa.c
@@ -2,16 +2,18 @@
 #include <stdio.h>
 
 int main() {
-	printf("codando em c");
+
+	//declarando
+	int idade;
+
+	//declarando e inicializando
+	char nome[10] = {"Silas"};
+
+	idade = 15;
+	printf("idade: %d, nome: %s\n", idade, nome);
+
+
+	printf("codando em c\n");
 	return 0;
 }
-
-
-
-
-
-
-
-
-
 
---
+++ 0001teste2/teste2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	printf("algo");
+	return 0;
+}
+
