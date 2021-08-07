7980d20d53776c411ed9ed976870aafbd3e20b51
codingscode <heronnestg@hotmail.com>
2021-08-07 20:22:09 -0300

    parte 6

--- 0002teste3/quest04.c
+++ 0002teste3/quest04.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
-
 #include <math.h>
-#include <stdlib.h>
 
 
 int main() {
 	double x = 5.5;
-	   double y = 4.0;
-	   double p;
-	   p = pow(x, y);
-	   printf("The value : %lf", p);
-	   return 0;
+	double y = 4.0;
+	double p;
+	p = pow (x, y);
+	printf("The value : %lf \n", p);
 
 
+	printf("algo\n");
+	printf("pot: %lf \n", pow(2, 3));
+	return 0;
+
+	/*
+     no terminal do arquivo usar:
+        gcc quest04.c -o saida.o -lm
+        ./saida.o
+	 */
 
 }
+
+
+
 /*
 
+	double x = 5.5;
+	double y = 4.0;
+	double p;
+	p = pow(x, y);
+	printf("The value : %lf \n", p);
+	return 0;
 
-
+--------------------------
    double num1 = 3.0;
    double quadrado = 0.0;
 
