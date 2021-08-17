22a82dd5cd957f9b8b64a5ce84374ae9896c71bd
codingscode <heronnestg@hotmail.com>
2021-08-17 12:00:20 -0300

    parte 35

---
+++ 0002teste3/teste0016_tipo_matrizes.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/*
+matrizes parte 1
+
+
+array multi-dimensional
+
+char array[3][4]
+
+*/
+
+
+int main() {
+	char nome[3][50];
+
+	for (int i = 0; i < 3; i++) {
+	   printf("qual seu nome?\n");
+	   gets(nome[i]);
+	}
+
+	printf("\n");
+
+	for (int i=0; i < 3; i++) {
+	   printf("Olá %s \n", nome[i]);
+	}
+
+   return 0;
+}
+
+
+
+/*
+ no terminal do arquivo usar:
+     gcc arquivo.c -o saida.o -lm
+     ./saida.o
+*/
+
+/*
+qual seu nome?
+samuel
+qual seu nome?
+israel
+qual seu nome?
+deborah
+
+Olá samuel
+Olá israel
+Olá deborah
+
+
+
+
+
+*/
+
