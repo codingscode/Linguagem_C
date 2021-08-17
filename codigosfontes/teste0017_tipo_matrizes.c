38ae088b7478e2b06071093c3eea292f0435cf07
codingscode <heronnestg@hotmail.com>
2021-08-17 12:20:44 -0300

    parte 36

---
+++ codigosfontes/teste0017_tipo_matrizes.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/*
+matrizes parte 2
+
+
+
+
+*/
+
+
+int main() {
+
+	int numeros[3][2];
+	numeros[0][0] = 1;
+	numeros[0][1] = 2;
+	numeros[1][0] = 3;
+	numeros[1][1] = 4;
+
+	for (int i=0; i < 2; i++) {
+	   for (int j =0; j < 2; j++) {
+	      printf("numero[%d][%d]: %d \n", i, j, numeros[i][j]);
+	   }
+	}
+
+	float valores[5][5];
+    // fazer com esse
+
+   return 0;
+}
+
+
+
+/*
+ no terminal do arquivo usar:
+     gcc arquivo.c -o saida.o -lm
+     ./saida.o
+*/
+
+/*
+
+
+
+
+
+
+
+*/
+
