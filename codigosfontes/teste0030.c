855f23d49edf9f931c2ae985786c31e53b032eca
codingscode <heronnestg@hotmail.com>
2021-08-19 20:47:25 -0300

    parte 50

---
+++ codigosfontes/teste0030.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+
+/*
+
+
+
+
+*/
+
+
+
+int main() {
+   // fopen(nome_do_arquivo, forma_de_abertura)
+   /*
+	w -> abrir o arquivo para escrita (se ele já existir, seu conteudo será sobrescrita)
+	r -> abrir o arquivo para leitura
+	wa -> abrir o arquivo e acrescenta conteudo.
+   */
+
+   FILE *arquivo;
+   char c;
+
+   arquivo = fopen("teste0030_arquivo.txt", "r");
+
+   if (arquivo) {
+      while ((c = getc(arquivo)) != EOF) {  // EOF -> end of file
+         printf("%c", c);
+      }
+   }
+   else {
+      printf("não achei o arquivo\n");
+   }
+
+
+   fclose(arquivo);
+
+
+   return 0;
+}
+
+
+/*
+ no terminal do arquivo usar:
+     gcc arquivo.c -o saida.o -lm
+     ./saida.o
+*/
+
+/*
+aladin
+mel
+apollo
+
+jepeto
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
