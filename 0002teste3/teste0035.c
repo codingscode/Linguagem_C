cdfe5800978a7b3802c51e80d334341396fc9bc6
codingscode <heronnestg@hotmail.com>
2021-08-20 10:40:44 -0300

    parte 55

---
+++ 0002teste3/teste0035.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+
+/*
+
+
+*/
+
+int main() {
+
+   FILE *arquivo;
+   char fruta[10];
+
+   arquivo = fopen("teste0035_frutas.txt", "w");  // write
+
+   if (arquivo) {
+      printf("Informe uma fruta, ou 0 para sair:\n");
+	  fgets(fruta, 10, stdin);
+	  while (fruta[0] != '0') {
+	     fputs(fruta, arquivo);
+	     printf("informe uma fruta, ou 0 para sair:\n");
+	     fgets(fruta, 10, stdin);
+	  }
+   }
+   else {
+	  printf("não foi possível criar o arquivo.");
+   }
+
+   fclose(arquivo);
+
+   return 0;
+}
+
+
+/*
+ no terminal do arquivo usar:
+     gcc arquivo.c -o saida.o -lm
+     ./saida.o
+*/
+
+/*
+Informe uma fruta, ou 0 para sair:
+goiaba
+informe uma fruta, ou 0 para sair:
+jaca
+informe uma fruta, ou 0 para sair:
+maçã
+informe uma fruta, ou 0 para sair:
+graviola
+informe uma fruta, ou 0 para sair:
+0
+
+
+
+*/
+
