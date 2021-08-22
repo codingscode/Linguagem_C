6b98526cb57ddadc68d8cb622a74cfb011c5db74
codingscode <heronnestg@hotmail.com>
2021-08-22 11:15:12 -0300

    parte 67

--- codigosfontes/teste0048_union.c
+++ codigosfontes/teste0048_union.c
@@ -14,18 +14,23 @@ union numeros {
 
 int main() {
    n.num1 = 6;
+   printf("valor de num1 é %d\n", n.num1);
    n.num2 = 8;
+   printf("valor de num2 é %d\n", n.num2);
    n.num3 = 3;
+   printf("valor de num3 é %d\n", n.num3);
    n.num4 = 1;
+   printf("valor de num4 é %d\n", n.num4);
    n.num5 = 5;
+   printf("valor de num5 é %d\n\n\n", n.num5);
 
    printf("valor de num1 é %d\n", n.num1);
    printf("valor de num2 é %d\n", n.num2);
    printf("valor de num3 é %d\n", n.num3);
    printf("valor de num4 é %d\n", n.num4);
-   printf("valor de num5 é %d\n", n.num5);
+   printf("valor de num5 é %d\n\n", n.num5);
 
-
+   printf("o 'n' ocupa %ld bytes\n", sizeof(n));
 
    return 0;
 }
@@ -38,11 +43,20 @@ int main() {
 */
 
 /*
+valor de num1 é 6
+valor de num2 é 8
+valor de num3 é 3
+valor de num4 é 1
+valor de num5 é 5
+
+
 valor de num1 é 5
 valor de num2 é 5
 valor de num3 é 5
 valor de num4 é 5
 valor de num5 é 5
+
+o 'n' ocupa 4 bytes
 
 
 
