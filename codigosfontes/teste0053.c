7bdb427fbb52b3dd652c36a4bcd6df11c9c4ce5c
codingscode <heronnestg@hotmail.com>
2021-08-23 09:55:10 -0300

    parte 72

---
+++ codigosfontes/teste0053.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+/*
+ifndef - diretiva de compilação
+
+
+*/
+//constantes
+#define PI 3.14
+
+
+int main() {
+   int valor = 8;
+   valor = 400;
+
+
+   printf("valor: %d\n", valor);
+   printf("PI: %f\n", PI);
+
+
+   return 0;
+}
+
+
+/*
+ no terminal do arquivo usar:
+     gcc arquivo.c -o saida.o -lm
+     ./saida.o
+*/
+
+/*
+valor: 400
+PI: 3.140000
+
+
+
+
+
+
+
+*/
+
