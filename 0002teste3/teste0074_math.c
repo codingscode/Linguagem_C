d7b8220736c573f2d4c118bce038f945c4efbd4d
codingscode <heronnestg@hotmail.com>
2021-08-25 11:06:11 -0300

    parte 93

---
+++ 0002teste3/teste0074_math.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+/*
+
+
+
+
+*/
+
+
+int main() {
+
+
+
+
+   return 0;
+}
+
+
+/*
+ no terminal do arquivo usar:
+     gcc arquivo.c -o saida.o -lm
+     ./saida.o
+*/
+
+/*
+
+
+
+
+
+
+
+
+
+
+*/
+
