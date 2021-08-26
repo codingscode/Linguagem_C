3f00003c6658500f93e816e1eb1c493f7f34e50a
codingscode <heronnestg@hotmail.com>
2021-08-26 11:31:12 -0300

    parte 96

---
+++ codigosfontes/teste0076_ctype.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <ctype.h>
+
+
+
+/*
+ctype.h
+
+teste de tipos de caracteres
+
+isalnum() -> verifica se o caractere é alfanumerico
+isalpha() -> '' se o caractere é alfabetico
+isdigit() -> " se o caractere é uma digito decimal
+ispunct() -> '' se o caractere é uma pontuação (?!.)
+isspace() -> "" se é um espaço ( )
+isupper() -> "" é maiuscula
+islower() -> "" é minuscula
+tolower() -> converte para minuscula
+toupper() -> converte para maiuscula
+
+
+*/
+
+void verificar(char carac) {
+   if (isalnum(carac)) {
+      printf("%c é alfanumérico.\n", carac);
+
+      if (isalpha(carac)) {
+	     printf("%c é alfabetico.\n", carac);
+
+	     if (isupper(carac)) {
+	        printf("%c é maiusculo.\n", carac);
+	     }
+	     else {
+	    	printf("%c é minusculo.\n", carac);
+	     }
+	  }
+	  if (isdigit(carac)) {
+		 printf("%c é digito decimal.\n", carac);
+	  }
+
+   }
+   else if(ispunct(carac)) {
+      printf("%c é pontuação.\n", carac);
+   }
+   else {
+      printf("nenhuma das anteriores.\n");
+   }
+
+}
+
+
+int main() {
+   char caractere1 = '!';
+   char caractere2 = 'b';
+   char caractere3 = 'K';
+   char caractere4 = '8';
+
+
+   verificar(caractere1);
+   printf("---------------\n");
+   verificar(caractere2);
+   printf("---------------\n");
+   verificar(caractere3);
+   printf("---------------\n");
+   verificar(caractere4);
+   printf("---------------\n");
+   printf("---------------\n");
+
+
+   return 0;
+}
+
+
+/*
+ no terminal do arquivo usar:
+     gcc arquivo.c -o saida.o -lm
+     ./saida.o
+*/
+
+/*
+
+
+
+
+
+
+
+
+
+
+*/
+
