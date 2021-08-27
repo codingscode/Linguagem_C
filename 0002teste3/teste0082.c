e50d80517c9bedb9d8f514ad3dc7c1fdc6fa4b55
codingscode <heronnestg@hotmail.com>
2021-08-27 10:41:18 -0300

    parte 101

---
+++ 0002teste3/teste0082.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+/*
+string.h
+
+operações em strings
+
+strcpy() -> copia de string
+strcat() -> concatena strings
+strcmp() -> compara duas strings
+strchr() -> localiza a primeira ocorrencia de caractere1actere em uma string
+
+
+
+
+*/
+
+
+
+
+int main() {
+
+
+   char string1[] = "pedro@gmail.com";
+   char caractere1 = '@';
+
+   char *retorno = strchr(string1, caractere1);
+
+   printf("A partir do '%c' está '%s'\n", caractere1, retorno);
+
+   return 0;
+}
+
+
+/*
+ no terminal do arquivo usar:
+     gcc arquivo.c -o saida.o -lm
+     ./saida.o
+*/
+
+/*
+A partir do '@' está '@gmail.com'
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
