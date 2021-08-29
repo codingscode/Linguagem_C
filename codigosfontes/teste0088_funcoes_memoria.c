e62bcb3a03c10f12a5260a5748ea39e9ad3f4da5
codingscode <heronnestg@hotmail.com>
2021-08-29 12:27:27 -0300

    parte 108

---
+++ codigosfontes/teste0088_funcoes_memoria.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <time.h>
+
+
+
+/*
+funções para alocação de memória
+
+
+
+
+
+
+*/
+
+
+
+
+int main() {
+   int numero = 9;
+
+   printf("a variavel numero vale '%d' e ocupa %ld bytes.\n ", numero, sizeof(numero));
+
+
+
+   return 0;
+}
+
+
+/*
+ no terminal do arquivo usar:
+     gcc arquivo.c -o saida.o -lm
+     ./saida.o
+*/
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
