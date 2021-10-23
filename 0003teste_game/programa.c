24226b3ba1ef4703dbc6cdf26e882fdef02a3697
codingscode <heronnestg@hotmail.com>
2021-10-23 11:56:45 -0300

    parte 132

--- 0003teste_game/programa.c
+++ 0003teste_game/programa.c
@@ -35,19 +35,74 @@ void jogar() {
 }
 
 void mostrarInfo(Calcular calc) {
-
+   char op[25];
+   
+   if (calc.operacao == 0) {  // 0 == somar
+      sprintf(op, "Somar");
+   }
+   else if (calc.operacao == 1) {
+      sprintf(op, "Diminuir");
+   }
+   else if (calc.operacao == 2) {
+      sprintf(op, "Multiplicar");
+   }
+   else {
+      sprintf(op, "Operação desconhecida");
+   }
+   printf("Valor 1: %d \nValor 2: %d\nDificuldade: %d \nOperacao: %s", calc.valor1, calc.valor2, calc.dificuldade, op);
+   
+   
 }
 
 int somar(int resposta, Calcular calc) {
-
+   int resultado = calc.valor1 + calc.valor2;
+   calc.resultado = resultado;
+   int certo = 0;  // 0 == errou, 1 == acertou
+   
+   if (resposta == calc.resultado) {
+      printf("Resposta correta!\n");
+      certo = 1;
+   }
+   else {
+      printf("Resposta errada!\n");
+   }
+   
+   printf("%d + %d = %d\n", calc.valor1, calc.valor2, calc.resultado);
+   return certo;
 }
 
 int diminuir(int resposta, Calcular calc) {
-
+   int resultado = calc.valor1 - calc.valor2;
+   calc.resultado = resultado;
+   int certo = 0;  // 0 == errou, 1 == acertou
+   
+   if (resposta == calc.resultado) {
+      printf("Resposta correta!\n");
+      certo = 1;
+   }
+   else {
+      printf("Resposta errada!\n");
+   }
+   
+   printf("%d - %d = %d\n", calc.valor1, calc.valor2, calc.resultado);
+   return certo;
 }
 
 int multiplicar(int resposta, Calcular calc) {
-
+   int resultado = calc.valor1 * calc.valor2;
+   calc.resultado = resultado;
+   int certo = 0;  // 0 == errou, 1 == acertou
+   
+   if (resposta == calc.resultado) {
+      printf("Resposta correta!\n");
+      certo = 1;
+   }
+   else {
+      printf("Resposta errada!\n");
+   }
+   
+   printf("%d * %d = %d\n", calc.valor1, calc.valor2, calc.resultado);
+   return certo;
 }
 
 
