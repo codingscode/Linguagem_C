4805346f175851d321d5eb868d3e17793940b599
codingscode <heronnestg@hotmail.com>
2021-10-27 12:10:18 -0300

    parte 134

--- 0003teste_game/programa.c
+++ 0003teste_game/programa.c
@@ -20,7 +20,7 @@ int pontos = 0;
 int main() {
 
    // deve ser chamado apenas 1 vez
-   srand(time(NULL)); 
+   srand(time(NULL));  // nao é erro
 
 
    jogar();
@@ -76,25 +76,25 @@ void jogar() {
       printf("%d + %d :\n", calc.valor1, calc.valor2);
       scanf("%d", &resposta);
       
-      if (somar(resposta, calc)) {
+      if (somar(resposta, calc)) {  // nao é erro
          pontos += 1;
          printf("Você tem %d ponto(s).\n", pontos);
       }
    } //diminuir
    else if (calc.operacao == 1) {
-      printf("%d - %d\n", calc.valor1, calc.valor2);
+      printf("%d - %d:\n", calc.valor1, calc.valor2);
       scanf("%d", &resposta);
       
-      if (diminuir(resposta, calc)) {
+      if (diminuir(resposta, calc)) {  // nao é erro
          pontos += 1;
          printf("Você tem %d ponto(s).\n", pontos);
       }
    }//multiplicar
    else if (calc.operacao == 2) {
-      printf("%d * %d", calc.valor1, calc.valor2);
+      printf("%d * %d:\n", calc.valor1, calc.valor2);
       scanf("%d", &resposta);
       
-      if (multiplicar(resposta, calc)) {
+      if (multiplicar(resposta, calc)) {  // nao é erro
          pontos += 1;
          printf("Você tem %d ponto(s).\n", pontos);
       }
@@ -102,6 +102,9 @@ void jogar() {
    else {
       printf("A operação %d não é reconhecida.\n", calc.operacao);
    }
+
+   //mostrarInfo
+   mostrarInfo(calc);
 
    // recomeçar o jogo?
    printf("Deseja continuar jogando? [1 - sim, 0 - não]\n");
@@ -135,7 +138,7 @@ void mostrarInfo(Calcular calc) {
    else {
       sprintf(op, "Operação desconhecida");
    }
-   printf("Valor 1: %d \nValor 2: %d\nDificuldade: %d \nOperacao: %s", calc.valor1, calc.valor2, calc.dificuldade, op);
+   printf("Valor 1: %d \nValor 2: %d\nDificuldade: %d \nOperacao: %s\n", calc.valor1, calc.valor2, calc.dificuldade, op);
    
    
 }
@@ -203,7 +206,66 @@ int multiplicar(int resposta, Calcular calc) {
 */
 
 /*
-
+Informe o nível de dificuldade desejado [1, 2, 3 ou 4]:
+1
+Informe o resultado para a seguinte operação:
+9 + 5 :
+14
+Resposta correta!
+9 + 5 = 14
+Você tem 1 ponto(s).
+Deseja continuar jogando? [1 - sim, 0 - não]
+1
+Informe o nível de dificuldade desejado [1, 2, 3 ou 4]:
+1
+Informe o resultado para a seguinte operação:
+2 - 7:
+-5
+Resposta correta!
+2 - 7 = -5
+Você tem 2 ponto(s).
+Deseja continuar jogando? [1 - sim, 0 - não]
+1
+Informe o nível de dificuldade desejado [1, 2, 3 ou 4]:
+1
+Informe o resultado para a seguinte operação:
+7 - 7:
+3
+Resposta errada!
+7 - 7 = 0
+Deseja continuar jogando? [1 - sim, 0 - não]
+1
+Informe o nível de dificuldade desejado [1, 2, 3 ou 4]:
+2
+Informe o resultado para a seguinte operação:
+85 - 48:
+37
+Resposta correta!
+85 - 48 = 37
+Você tem 3 ponto(s).
+Deseja continuar jogando? [1 - sim, 0 - não]
+1
+Informe o nível de dificuldade desejado [1, 2, 3 ou 4]:
+3
+Informe o resultado para a seguinte operação:
+360 + 704 :
+1064
+Resposta correta!
+360 + 704 = 1064
+Você tem 4 ponto(s).
+Deseja continuar jogando? [1 - sim, 0 - não]
+1
+Informe o nível de dificuldade desejado [1, 2, 3 ou 4]:
+4
+Informe o resultado para a seguinte operação:
+2808 * 9240:
+123
+Resposta errada!
+2808 * 9240 = 25945920
+Deseja continuar jogando? [1 - sim, 0 - não]
+0
+Você finalizou com 4 ponto(s).
+Até a próxima.
 
 
 
