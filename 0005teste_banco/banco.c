07e9309bbf6be2414cb068ed25080c8aeee63478
codingscode <heronnestg@hotmail.com>
2021-10-31 10:57:37 -0300

    parte 143

--- 0005teste_banco/banco.c
+++ 0005teste_banco/banco.c
@@ -55,6 +55,50 @@ int main() {
 
 
 void menu() {
+   int opcao = 0;
+   printf("===================================\n");
+   printf("=============== ATM ===============\n");
+   printf("============ Geek Bank ============\n");
+   printf("===================================\n");
+   
+   printf("Selecione uma opção no menu:\n");
+   printf("1 - Criar conta.\n");
+   printf("2 - Efetuar saque.\n");
+   printf("3 - Efetuar depósito.\n");
+   printf("4 - Efetuar transferência.\n");
+   printf("5 - Listar contas.\n");
+   printf("6 - Sair.\n");                  
+
+   scanf("%d", &opcao);
+   getchar();
+   
+   switch(opcao) {
+      case 1:
+         criarConta();
+         break;
+      case 2:
+         efetuarSaque();
+         break;
+      case 3:
+         efetuarDeposito();
+         break;
+      case 4:
+         efetuarTransferencia();
+         break;
+      case 5:
+         listarContas();
+         break;
+      case 6:
+         printf("\nAté a próxima!\n");
+         sleep(2);
+         exit(0);
+      default:
+         printf("Opção inválida.\n");
+         sleep(2);
+         menu();
+         break;
+   }
+
 
 }
 
