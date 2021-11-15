da7ce2149245fe0ab5bbbe9658aa2ce08cf6957e
codingscode <heronnestg@hotmail.com>
2021-11-15 13:06:50 -0300

    rev-21

--- 0006teste_revisao/rev01.c
+++ 0006teste_revisao/rev01.c
@@ -7,66 +7,87 @@
 
 
 int main() {
-   // 3, 8, 5
-   int temporario = 0;
-   int teste[3] = {1, 2, 3};
-   int numeros[3];
+	char chegada[] = "10:13:25";
+	int tam_ini = strlen(chegada);
+	char delim[] = ":";
+
+	char *ptr = strtok(chegada, delim);
+
+	while(ptr != NULL) {
+		printf("'%s'\n", ptr);
+		ptr = strtok(NULL, delim);
+	}
+
+	/* This loop will show that there are zeroes in the str after tokenizing */
+	for (int i = 0; i < tam_ini; i++) {
+		printf("%d ", chegada[i]); /* Convert the character to integer, in this case
+							   the character's ASCII equivalent */
+	}
+	printf("\n");
+
+	return 0;
+}
+
+// https://www.codingame.com/playgrounds/14213/how-to-play-with-strings-in-c/string-split
+// https://www.geeksforgeeks.org/convert-a-string-to-integer-array-in-c-c/
+
+/*
+
+   while(ptr != NULL)
+   	{
+   		printf("'%s'\n", ptr);
+   		ptr = strtok(NULL, delim);
+   	}
 
 
-   printf("digite tres numeros inteiros um abaixo do outro:\n");
-   scanf("%d %d %d", &numeros[0], &numeros[1], &numeros[2]);
+   	for (int i = 0; i < init_size; i++)
+   	{
+   		printf("%d ", str[i]);
 
-   for (int i=0; i < 3; i++) {
-      printf("%dº numero: %d\n", i+1, numeros[i]);
+   	}
+   	printf("\n");
+
+char* chegada = "45", saida;
+int numero = atoi(chegada);
+
+printf("numero: %d\n", numero);
+
+
+
+*/
+
+/*
+
+
+   char* chegada = "10:13:25", saida;
+   char* delimitador = ":";
+   int numero = atoi(chegada);
+
+   char *ponteiro = strtok(chegada, delimitador);
+//   char *ptr = strtok(str, delim);
+
+
+   //printf("numero: %d\n", numero);
+
+   while (ponteiro != NULL) {
+      printf("'%s'\n", ponteiro);
+      ponteiro = strtok(NULL, delimitador);
    }
 
-   long int comp_numeros = sizeof(numeros)/sizeof(int);
-
-   printf("comprimento de numeros: %ld\n", comp_numeros);
-
-
-   // ordenando o vetor em ordem crescente
-   for (int i = 0; i < comp_numeros; i++) {
-      for (int j = i+1; j < comp_numeros; j++) {
-         if(numeros[i] > numeros[j]) {
-            temporario = numeros[i];
-            numeros[i] = numeros[j];
-            numeros[j] = temporario;
-         }
-      }
+   for (int i = 0; i < strlen(chegada); i++) {
+      printf("%d ", chegada[i]);
    }
 
    printf("\n");
 
-   // mostrando elementos do vetor depois de ordenado
-   printf("elementos do vetor em ordem crescente:\n");
-   for (int i = 0; i < comp_numeros; i++) {
-      printf("%d ", numeros[i]);
-   }
 
-   printf("\n");
+
 
 
    printf("fim\n");
    return 0;
-}
 
 
-
-/*
-
-digite tres numeros inteiros um abaixo do outro:
-10
-2
-8
-1º numero: 10
-2º numero: 2
-3º numero: 8
-comprimento de numeros: 3
-
-elementos do vetor em ordem crescente:
-2 8 10
-fim
 
 
 
