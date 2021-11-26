68408f7a07e07865135a42e59bc41cc621bedf07
codingscode <heronnestg@hotmail.com>
2021-11-26 10:41:21 -0300

    rev-28

--- 0006teste_revisao/rev01.c
+++ 0006teste_revisao/rev01.c
@@ -1,86 +1,43 @@
 #include <stdio.h>
-#include <string.h>
+
 
 
 
 /*
-structs -> estrutura
+union ->
+
+
 
 */
-
-struct st_contato {
-   char nome[100];
-   int ano_nascimento;
-   char telefone[20];
-   char email[100];
-};
-
-struct st_agenda {
-   struct st_contato contatos[100];
-} agenda;
 
 
 int main() {
 
-   for (int i = 0; i < 2; i++) {
-      printf("informe o nome do contato:\n");
-      fgets(agenda.contatos[i].nome, 100, stdin);
+   int numero = 42;
+   float nota = 5.8;
+   char letra = 'k';
+   double nota2 = 12.5;
+   char nome[] = "teixeira";
 
-      printf("informe o ano de nascimento do contato:\n");
-      scanf("%d", &agenda.contatos[i].ano_nascimento);
-      getchar();  // evita problema scanf e fgets
 
-      printf("informe o telefone do contato:\n");
-      fgets(agenda.contatos[i].telefone, 20, stdin);
+   printf("variavel 'numero' seu valor: %d, ocupa %ld bytes.\n", numero, sizeof(numero));
+   printf("variavel 'nota' seu valor: %.2f, ocupa %ld bytes.\n", nota, sizeof(nota));
+   printf("variavel 'letra' seu valor: %c, ocupa %ld bytes.\n", letra, sizeof(letra));
+   printf("variavel 'nota2' seu valor: %.2lf, ocupa %ld bytes.\n", nota2, sizeof(nota2));
+   printf("variavel 'nome' seu valor: %s, ocupa %ld bytes.\n", nome, sizeof(nome));
 
-      printf("informe o email do contato:\n");
-      fgets(agenda.contatos[i].email, 100, stdin);
-      printf("\n");
-   }
-
-   for (int i = 0; i < 2; i++) {
-      printf("\n========== dados do contato %d ==============\n", i + 1);
-      printf("nome: %s\n", strtok(agenda.contatos[i].nome, "\n") );
-      printf("ano de nascimento: %d\n", agenda.contatos[i].ano_nascimento );
-      printf("telefone: %s\n", strtok(agenda.contatos[i].telefone, "\n") );
-      printf("email: %s\n", strtok(agenda.contatos[i].email, "\n") );
-
-   }
-
+   printf("sizef(int): %ld\n", sizeof(int));
    return 0;
 }
 
 
 /*
-informe o nome do contato:
-maria sales
-informe o ano de nascimento do contato:
-1990
-informe o telefone do contato:
-32323232
-informe o email do contato:
-maria@gmail.com
-informe o nome do contato:
-bob allan
-informe o ano de nascimento do contato:
-2001
-informe o telefone do contato:
-21212121
-informe o email do contato:
-bob21@gmail.com
-
-========== dados do contato 1 ==============
-nome: maria sales
-ano de nascimento: 1990
-telefone: 32323232
-email: maria@gmail.com
-
-========== dados do contato 2 ==============
-nome: bob allan
-ano de nascimento: 2001
-telefone: 21212121
-email: bob21@gmail.com
-
+variavel 'numero' seu valor: 42, ocupa 4 bytes.
+variavel 'nota' seu valor: 5.80, ocupa 4 bytes.
+variavel 'letra' seu valor: k, ocupa 1 bytes.
+variavel 'nota2' seu valor: 12.50, ocupa 8 bytes.
+variavel 'nome' seu valor: teixeira, ocupa 9 bytes.
+sizef(int): 4
 
 
 
--- codigosfontes/teste0044_union.c
+++ codigosfontes/teste0044_union.c
@@ -13,20 +13,21 @@ union ->
 
 int main() {
 
-   int numero = 42;
-   float nota = 5.8;
-   char letra = 'k';
-   double nota2 = 12.5;
+	int numero = 42;
+	float nota = 5.8;
+	char letra = 'k';
+	double nota2 = 12.5;
+	char nome[] = "teixeira";
 
 
-   printf("variavel 'numero' seu valor: %d, ocupa %ld bytes.\n", numero, sizeof(numero));
-   printf("variavel 'nota' seu valor: %.2f, ocupa %ld bytes.\n", nota, sizeof(nota));
-   printf("variavel 'letra' seu valor: %c, ocupa %ld bytes.\n", letra, sizeof(letra));
-   printf("variavel 'nota2' seu valor: %.2lf, ocupa %ld bytes.\n", nota2, sizeof(nota2));
+	printf("variavel 'numero' seu valor: %d, ocupa %ld bytes.\n", numero, sizeof(numero));
+	printf("variavel 'nota' seu valor: %.2f, ocupa %ld bytes.\n", nota, sizeof(nota));
+	printf("variavel 'letra' seu valor: %c, ocupa %ld bytes.\n", letra, sizeof(letra));
+	printf("variavel 'nota2' seu valor: %.2lf, ocupa %ld bytes.\n", nota2, sizeof(nota2));
+	printf("variavel 'nome' seu valor: %s, ocupa %ld bytes.\n", nome, sizeof(nome));
 
-
-   printf("sizef(int): %ld\n", sizeof(int));
-   return 0;
+	printf("sizef(int): %ld\n", sizeof(int));
+	return 0;
 }
 
 
@@ -41,6 +42,7 @@ variavel 'numero' seu valor: 42, ocupa 4 bytes.
 variavel 'nota' seu valor: 5.80, ocupa 4 bytes.
 variavel 'letra' seu valor: k, ocupa 1 bytes.
 variavel 'nota2' seu valor: 12.50, ocupa 8 bytes.
+variavel 'nome' seu valor: teixeira, ocupa 9 bytes.
 sizef(int): 4
 
 
